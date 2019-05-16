d76b47eff433f7b98a76b24a64883e878e2c05e3
allenleung86 <londalonda@qq.com>
2019-05-16 10:35:23 +0800

    提交camunda工作流的初始版本

---
+++ src/test/java/com/camunda/bpm/getstarted/loanapproval/WebappExampleProcessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.camunda.bpm.getstarted.loanapproval;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebappExampleProcessApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
